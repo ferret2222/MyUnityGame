6569a69cf768c9403625718af3a19676e9619587
jana.b <2024.gme.janabuh@gmail.com>
2025-04-15 11:40:14 +0200

    Change BaseCharacterController & add Camera

--- Assets/BaseCharacterController.cs
+++ Assets/BaseCharacterController.cs
@@ -5,8 +5,21 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class BaseCharacterController : MonoBehaviour
 {
+    private Vector2 movementInput;
+    [SerializeField] float movementSpeed;
+
+    
     public void Movement(CallbackContext ctx)
     {
-    Debug.Log($"Context: {ctx.ReadValue<Vector2>()}");
+        // Debug.Log($"Context: {ctx.ReadValue<Vector2>()}");
+        movementInput = ctx.ReadValue<Vector2>();
+    }
+
+    /* multiple row commentsssssssssssssssssssssssss are made like thisssssssssss okayyyyyyyyyyyyyyy, put themmmmmmmmmmmmmmmmmm
+     heeeeereeeeee */
+    private void Update()
+    {
+        
+        transform.position += new Vector3(movementInput.x, movementInput.y, 0) * Time.deltaTime * movementSpeed; // comment
     }
 }
