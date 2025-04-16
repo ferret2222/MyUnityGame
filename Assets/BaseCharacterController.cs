36ba33da1c7de435208bf292ef7c48870fe7a29f
jana.b <2024.gme.janabuh@gmail.com>
2025-04-16 12:07:19 +0200

    Added Tileset, Tilemap, Collision, Rigidbody

--- Assets/BaseCharacterController.cs
+++ Assets/BaseCharacterController.cs
@@ -3,23 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.InputSystem.InputAction;
 
+//BaseCharacter is the base of a character
 public class BaseCharacterController : MonoBehaviour
 {
     private Vector2 movementInput;
-    [SerializeField] float movementSpeed;
+    [SerializeField] private float movementSpeed;
+    private Rigidbody2D rb;
 
-    
-    public void Movement(CallbackContext ctx)
+    private void Start()
     {
-        // Debug.Log($"Context: {ctx.ReadValue<Vector2>()}");
-        movementInput = ctx.ReadValue<Vector2>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    /* multiple row commentsssssssssssssssssssssssss are made like thisssssssssss okayyyyyyyyyyyyyyy, put themmmmmmmmmmmmmmmmmm
-     heeeeereeeeee */
-    private void Update()
+    /// <summary>
+    /// Movement is called by the input system when the player moves the joystick or presses the arrow keys
+    /// </summary>
+    /// <param name="ctx">Context provided by Unity Input</param>
+    public void Movement(CallbackContext ctx)
     {
-        
-        transform.position += new Vector3(movementInput.x, movementInput.y, 0) * Time.deltaTime * movementSpeed; // comment
+        //movementInput is set by unity events
+        movementInput = ctx.ReadValue<Vector2>(); //comment
+    }
+
+    //This is now a FIXEDupdate
+    private void FixedUpdate()
+    {
+        //transform.position += new Vector3(movementInput.x, movementInput.y, 0) * Time.deltaTime * movementSpeed;
+
+        transform.Translate(new Vector3(movementInput.x, movementInput.y, 0) * Time.deltaTime * movementSpeed);
+
+        //rb.velocity = new Vector3(movementInput.x, movementInput.y, 0) * movementSpeed;
+
+        //rb.AddForce(new Vector3(movementInput.x, movementInput.y, 0) * movementSpeed);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collison detetected with " + collision.gameObject.name);
     }
 }
